173e6abd961816773e0696f243467a3860c3761e
Daniel Levyson <levyson.daniel@gmail.com>
2021-11-19 10:05:13 +0700

    add logout

--- js/index.js
+++ js/index.js
@@ -3,12 +3,12 @@ let sudahlogintadi = false
 const checkLoginForm = document.querySelector('#checkLogin')
 checkLoginForm.addEventListener('submit', function(event) {
   event.preventDefault()
+
   const nama = document.querySelector('#nama')
   const email = document.querySelector('#email')
-  const banner = document.querySelector('.banner')
   const testing = document.querySelector('.logincontain')
-  const button = document.querySelector('#checkAnswer')
   const warning = document.createElement('h6')
+
   warning.innerHTML = 'Mohon isi nama dan alamat email anda!'
   
   if(nama.value === `` || email.value === ``) {
@@ -21,26 +21,54 @@ checkLoginForm.addEventListener('submit', function(event) {
     if (logintested === true) {
       testing.removeChild(document.querySelector("#warned"))
     }
-    let namauser = nama.value
-    let emailuser = email.value
+    let namauser = nama.value;
+    let emailuser = email.value;
 
     if (sudahlogintadi === false) {
-      banner.removeChild(document.querySelector('#mohonlogin'))
-
-      button.innerHTML = "Ubah Nama dan Email"
-
-      const afterLoginDiv = document.getElementById('after-login');
-
-      afterLoginDiv.classList.remove('hidden');
-      afterLoginDiv.getElementsByTagName("H4")[0].innerHTML = `Selamat datang ${namauser}!`
-      afterLoginDiv.getElementsByTagName('p')[0].innerHTML = `(dengan email: ${emailuser})`;
-
+      saveLoginInfo(nama.value, email.value);
+      handleLoggedInDisplay();
       sudahlogintadi = true
-      
     } else {
       document.getElementsByTagName("H4")[0].innerHTML = `Selamat datang ${namauser}!`
       document.getElementsByTagName("P")[1].innerHTML = `(dengan email: ${emailuser})`
     }
   }
+});
 
-})
+function logout() {
+  window.localStorage.clear();
+  location.reload();
+}
+
+function handleLoggedInDisplay() {
+  document.getElementById('checkLogin').classList.add('invisible');
+  document.querySelector('.banner').removeChild(document.querySelector('#mohonlogin'));
+  document.querySelector('#checkAnswer').innerHTML = "Ubah Nama dan Email";
+
+  const afterLoginDiv = document.getElementById('after-login');
+  const loginInfo = getLoginInfo();
+
+  afterLoginDiv.classList.remove('hidden');
+  afterLoginDiv.getElementsByTagName("H4")[0].innerHTML = `Selamat datang ${loginInfo.name}!`;
+  afterLoginDiv.getElementsByTagName('p')[0].innerHTML = `(dengan email: ${loginInfo.email})`;
+}
+
+function saveLoginInfo(name, email) {
+  window.localStorage.setItem('login.name', name);
+  window.localStorage.setItem('login.email', email);
+}
+
+function getLoginInfo() {
+  let result = null;
+
+  const email = window.localStorage.getItem('login.email');
+
+  if (email) {
+    result = {
+      name: window.localStorage.getItem('login.name'),
+      email
+    }
+  }
+
+  return result;
+}
